347704644182a6f0772f96cc0e51fa69e7cfd331
LennertS <39487878+LennertS@users.noreply.github.com>
2019-05-19 21:56:49 +0200

    updated "project" folder to version from 14 may

--- project/mhp-game/src/hellotvxlet/HelloTVXlet.java
+++ project/mhp-game/src/hellotvxlet/HelloTVXlet.java
@@ -1,6 +1,8 @@
 package hellotvxlet;
 
 import java.awt.Color;
+import java.awt.Image;
+import java.lang.Math;
 import javax.tv.xlet.*;
 import org.havi.ui.HScene;
 import org.havi.ui.HSceneFactory;
@@ -10,23 +12,41 @@ import org.havi.ui.HVisible;
 
 public class HelloTVXlet implements Xlet {
 
+    
+    static int cubeWidth = 200, cubeHeigth = 400;
+    static int sceneWidth = 800, sceneHeight = 600;
+    static Sound sound = new Sound();
   
     public HelloTVXlet() {
         
     }
 
     public void initXlet(XletContext context) {
-      HScene scene=HSceneFactory.getInstance().getDefaultHScene();
+        HScene scene=HSceneFactory.getInstance().getDefaultHScene();
       
-      HStaticText hst=new HStaticText("Hello World",100,100,200,200);
-      hst.setBackgroundMode(HVisible.BACKGROUND_FILL);
-      hst.setBackground(Color.BLUE);
-      
-     
-      scene.add(hst);
-      scene.setVisible(true);
-         
-      scene.validate();
+        HStaticText redCube = new HStaticText("Press 'Q'",(sceneWidth - cubeWidth)/2, (sceneHeight - cubeHeigth)/2, cubeWidth, cubeHeigth);
+        redCube.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        redCube.setBackground(Color.RED);
+        HStaticText greenCube = new HStaticText("Press 'S'",(sceneWidth - cubeWidth)/2, (sceneHeight - cubeHeigth)/2, cubeWidth, cubeHeigth);
+        greenCube.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        greenCube.setBackground(Color.GREEN);
+        HStaticText blueCube = new HStaticText("Press 'D'",(sceneWidth - cubeWidth)/2, (sceneHeight - cubeHeigth)/2, cubeWidth, cubeHeigth);
+        blueCube.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        blueCube.setBackground(Color.BLUE);
+        
+        HStaticText[] cubes = {redCube, greenCube, blueCube};
+        HStaticText activeCube = cubes[(int)Math.floor(Math.random()*cubes.length)];
+           
+        scene.add(activeCube);
+
+        scene.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        scene.setBackground(Color.BLACK);
+        scene.setVisible(true);
+
+        sound.PlaySound();
+        
+        scene.validate();
+        
  
       
      
